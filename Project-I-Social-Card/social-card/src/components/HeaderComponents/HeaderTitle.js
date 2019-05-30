f310288cae895638967d6c3eda151fb6be08a115
George Adams <gadams@Georges-Air.local>
2019-05-30 18:55:47 -0500

    v.1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderTitle() {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <p>@Lambda School </p>
+    </div>
+  );
+}
+
+export default HeaderTitle;
